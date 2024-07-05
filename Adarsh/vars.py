381365c657841ce3f7e66290df8d8ef523e51e0a
admin44449999 <108731844+admin44449999@users.noreply.github.com>
2024-07-05 21:41:43 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -1,40 +1,34 @@
-# (c) adarsh-goel
-import os
+
+
 from os import getenv, environ
 from dotenv import load_dotenv
 
-
-
 load_dotenv()
 
+
 class Var(object):
-    MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID'))
-    API_HASH = str(getenv('API_HASH'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN'))
-    name = str(getenv('name', 'filetolinkbot'))
-    SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
-    WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
-    PORT = int(getenv('PORT', 8080))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
-    NO_PORT = bool(getenv('NO_PORT', False))
-    APP_NAME = str(getenv('APP_NAME'))
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
-    if 'DYNO' in environ:
+    API_ID = int(getenv("API_ID","23080322"))
+    API_HASH = str(getenv("API_HASH","b3611c291bf82d917637d61e4a136535"))
+    BOT_TOKEN = str(getenv("BOT_TOKEN","7494418251:AAEoKXkpfO1xEeDbbGW2jjy33MVfjBK1RiY"))
+    SESSION_NAME = str(getenv("SESSION_NAME", "F2LxBot"))
+    SLEEP_THRESHOLD = int(getenv("SLEEP_THRESHOLD", "60"))
+    WORKERS = int(getenv("WORKERS", "4"))
+    BIN_CHANNEL = int(getenv("BIN_CHANNEL"," -1002226307271"))
+    PORT = int(getenv("PORT", 8080))
+    BIND_ADRESS = str(getenv("WEB_SERVER_BIND_ADDRESS", "https://miniature-gae-telegram-bots-7a6a60ef.koyeb.app/"))
+    OWNER_ID = int(getenv("OWNER_ID", 6214889840))
+    NO_PORT = bool(getenv("NO_PORT", False))
+    APP_NAME = None
+    OWNER_USERNAME = str(getenv("OWNER_USERNAME","aryanchy449"))
+    if "DYNO" in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME'))
-    
+        APP_NAME = str(getenv("APP_NAME"))
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-    HAS_SSL=bool(getenv('HAS_SSL',False))
-    if HAS_SSL:
-        URL = "https://{}/".format(FQDN)
-    else:
-        URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+    FQDN = str(getenv("FQDN","https://miniature-gae-telegram-bots-7a6a60ef.koyeb.app/")) if not ON_HEROKU or getenv("FQDN","https://miniature-gae-telegram-bots-7a6a60ef.koyeb.app/") else APP_NAME+".herokuapp.com"
+    URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
+        "http://{}:{}/".format(FQDN, PORT)
+    DATABASE_URL = str(getenv("DATABASE_URL"))
+    UPDATES_CHANNEL = str(getenv("UPDATES_CHANNEL", None))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()))
+    
